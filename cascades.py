eb8aa6a7b277cf497e098d8c5679bf4e75b52bcf
Matthew Ho <msho@ucsb.edu>
2021-02-20 15:41:43 -0800

    centroid tracker driver

--- cascades.py
+++ cascades.py
@@ -10,7 +10,7 @@ from collections import OrderedDict
 car_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'cars.xml')
 
 class CentroidTracker():
-	def __init__(self, max_disappeared = 50):
+	def __init__(self, maxDisappeared = 50):
 		self.nextObjectID = 0
 		self.objects = OrderedDict()
 		self.disappeared = OrderedDict()
@@ -19,7 +19,7 @@ class CentroidTracker():
 		self.objects[self.nextObjectID] = centroid
 		self.disappeared[self.nextObjectID] = 0
 		self.nextObjectID += 1
-	def register(self, objectID):
+	def deregister(self, objectID):
 		del self.objects[objectID]
 		del self.disappeared[objectID]
 	def update(self, rects):
@@ -32,19 +32,26 @@ class CentroidTracker():
 			return self.objects
 
 		#init array of input centroids for current frame
-		inputCentroids = np.zeroes((len(rects), 2), dtype="int")
+		inputCentroids = np.zeros((len(rects), 2), dtype="int")
 
 		#loop over each rectangle
 		for (i, (startX, startY, endX, endY)) in enumerate(rects):
+			'''
+			print("startX: ", startX)
+			print("startY: ", startY)
+			'''
 			#get centroid coords
 			cX = int((startX + endX) * 0.5)
 			cY = int((startY + endY) * 0.5)
 			inputCentroids[i] = (cX, cY)
+			#print("inputCentroids: ")
+			print(inputCentroids)
 
 		#if not currently tracking, register all centroids
 		if len(self.objects) == 0:
 			for i in range(len(inputCentroids)):
 				self.register(inputCentroids[i])
+
 		else:
 			# get objectIDs and their centroids
 			objectIDs = list(self.objects.keys())
@@ -78,6 +85,8 @@ class CentroidTracker():
 
 				# check if centroids appeared or disappeared
 				if D.shape[0] >= D.shape[1]:
+					# more centroids in previous frame, increment disappeared counts
+					# deregister as necessary
 					for row in unusedRows:
 						objectID = objectIDs[row]
 						self.disappeared[objectID] += 1
@@ -85,13 +94,17 @@ class CentroidTracker():
 						if self.disappeared[objectID] > self.maxDisappeared:
 							self.deregister(objectID)
 				else:
+					# more ceontroids in current frame, register them
 					for col in unusedCols:
 						self.register(inputCentroids[col])
-			return self.objects
+		return self.objects
 
 col_frames = os.listdir('frames')
 col_frames.sort(key = lambda f: int(re.sub('\D', '', f)))
 col_images=[]
+
+ct = CentroidTracker()
+
 for im in col_frames:
 	img = cv2.imread('frames/' + im)
 	col_images.append(img)
@@ -102,10 +115,32 @@ f, axarr = plt.subplots(len(frames), 1)
 for i in range(len(frames)):
 	frame = col_images[frames[i]]
 	cars = car_cascade.detectMultiScale(frame, 1.05, 2)
+	rects = []
+	for (x, y, w, h) in cars:
+		rect = (x, y, x + w, y + h)
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
+		rects.append(rect)
+	#print(rects)
+	objects = ct.update(rects)
+	#print(len(objects))
+	for (objectID, centroid) in objects.items():
+		cv2.putText(frame, "ID: " + str(objectID) , (centroid[0] - 10, centroid[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+			0.5, (0, 255, 0), 2)
+		#cv2.circle(frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1)
+	
+	
+
+'''
+for i in range(len(frames)):
+	frame = col_images[frames[i]]
+	cars = car_cascade.detectMultiScale(frame, 1.05, 2)
 	for (x, y, w, h) in cars:
 		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 	axarr[i].imshow(frame)
 	axarr[i].set_title('frame: ' + str(frames[i]))
+'''
 
 '''
 frame = col_images[frame1]
